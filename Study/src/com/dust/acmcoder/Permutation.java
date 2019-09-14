7df1a4a8212a5123059f984588ae78d2fed08a7d
luobin <1667918791@qq.com>
2019-09-14 21:07:35 +0800

    字符串的排列

--- Study/src/com/dust/acmcoder/Permutation.java
+++ Study/src/com/dust/acmcoder/Permutation.java
@@ -53,6 +53,6 @@ public class Permutation {
 
     public static void main(String[] args) {
         Permutation o = new Permutation();
-        System.out.println(o.Permutation("aaa").toString());
+        System.out.println(o.Permutation("abcd").toString());
     }
 }

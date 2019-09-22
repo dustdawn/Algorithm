d0670874f1a79519b6ad6a61ee75871f904a9a6b
luobin <1667918791@qq.com>
2019-09-22 12:07:02 +0800

    生产者消费者问题

---
+++ Study/src/com/dust/thread/producer_consumer/Main.java
@@ -0,0 +1,16 @@
+package com.dust.thread.producer_consumer;
+
+/**
+ * @author dustdawn
+ * @date 2019/9/22 11:42
+ */
+public class Main {
+    public static void main(String[] args) {
+        product pro = new product();
+        Thread t1 = new Thread(new producer(pro), "生产者线程");
+        Thread t2 = new Thread(new consumer(pro), "消费者线程");
+        t1.start();
+        t2.start();
+
+    }
+}

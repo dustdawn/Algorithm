26024de42d72b6b591d40a0603656e4d2dd0ab5d
luobin <1667918791@qq.com>
2019-09-24 10:51:42 +0800

    二叉树相关

---
+++ Study/src/com/dust/binaryTree/TreeNode.java
@@ -0,0 +1,22 @@
+package com.dust.binaryTree;
+
+/**
+ * @author dustdawn
+ * @date 2019/9/24 10:37
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+    TreeNode() {
+        val = 0;
+        left = null;
+        right = null;
+    }
+
+}

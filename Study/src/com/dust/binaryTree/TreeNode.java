f3dbd46b696f42008d3e3d386ebe11dcaa3edc10
luobin <1667918791@qq.com>
2019-10-01 10:38:36 +0800

    查找二叉树中序序列的下一节点

--- Study/src/com/dust/binaryTree/TreeNode.java
+++ Study/src/com/dust/binaryTree/TreeNode.java
@@ -8,15 +8,18 @@ public class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
+    TreeNode next;//指向父结点的指针
     TreeNode(int x) {
         val = x;
         left = null;
         right = null;
+        next = null;
     }
     TreeNode() {
         val = 0;
         left = null;
         right = null;
+        next = null;
     }
 
 }

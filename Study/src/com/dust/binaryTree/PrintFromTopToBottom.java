f349f99d939e633a8b31396fc68dc65571a3ec2b
luobin <1667918791@qq.com>
2019-10-01 10:58:22 +0800

    从上到下打印二叉树

---
+++ Study/src/com/dust/binaryTree/PrintFromTopToBottom.java
@@ -0,0 +1,48 @@
+package com.dust.binaryTree;
+
+/**
+ * @author dustdawn
+ * @date 2019/10/1 10:54
+ */
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 题目描述
+ * 从上往下打印出二叉树的每个节点，同层节点从左至右打印。
+ */
+public class PrintFromTopToBottom {
+    //队列实现层次遍历
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+
+
+
+        ArrayList list = new ArrayList();
+        if (root == null) {
+            return list;
+        }
+
+        TreeNode node = root;
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(node);
+
+        while (!queue.isEmpty()) {
+            node = queue.poll();
+            System.out.println(node.val + " ");
+
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+            list.add(node.val);
+        }
+        return list;
+
+    }
+
+}

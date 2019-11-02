89fd1591ecea494b113e75a28979881868661de2
luobin <1667918791@qq.com>
2019-11-02 15:06:06 +0800

    简单工厂模式

---
+++ Study/src/com/dust/DesignPatterns/Creational/SimpleFactory/ConcreteProductA.java
@@ -0,0 +1,12 @@
+package com.dust.DesignPatterns.Creational.SimpleFactory;
+
+/**
+ * @author dustdawn
+ * @date 2019/10/31 16:07
+ */
+public class ConcreteProductA extends Product {
+    @Override
+    public void methodDiff() {
+        //A业务方法
+    }
+}

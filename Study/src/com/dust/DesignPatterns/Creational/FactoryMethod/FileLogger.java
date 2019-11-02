582700e99adf4493a3e279e54287d6e1816da8ce
luobin <1667918791@qq.com>
2019-11-02 15:06:26 +0800

    工厂方法模式

---
+++ Study/src/com/dust/DesignPatterns/Creational/FactoryMethod/FileLogger.java
@@ -0,0 +1,16 @@
+package com.dust.DesignPatterns.Creational.FactoryMethod;
+
+/**
+ * @author dustdawn
+ * @date 2019/11/2 14:51
+ */
+
+/**
+ * 文件日志记录器，充当具体产品角色
+ */
+public class FileLogger implements Logger{
+    @Override
+    public void writeLog() {
+        System.out.println("文件日志记录");
+    }
+}

e7fbd6ebe8fc7117449802f2408e8f26ef4ec8eb
dustdawn <dustdawn@163.com>
2022-02-21 23:55:00 +0800

    阿里规范扫描

--- Study/src/com/dustdawn/acmcoder/ArraySearch.java
+++ Study/src/com/dustdawn/acmcoder/ArraySearch.java
@@ -1,17 +1,15 @@
 package com.dustdawn.acmcoder;
 
 /**
- * @author dustdawn
- * @date 2019/9/3 8:47
- */
-
-/**
  * 题目描述
  * 在一个二维数组中（每个一维数组的长度相同），
  * 每一行都按照从左到右递增的顺序排序，
  * 每一列都按照从上到下递增的顺序排序。
  * 请完成一个函数，输入这样的一个二维数组和一个整数，
  * 判断数组中是否含有该整数。
+ *
+ * @author dustdawn
+ * @date 2019/9/3 8:47
  */
 public class ArraySearch {
     //暴力二分法查询每一行 O(n* log n)
@@ -36,13 +34,19 @@ public class ArraySearch {
         return false;
     }*/
 
-    //矩阵是有序的：利用二维数组由上到下，由左到右递增的规律。
-    //那么选取右上角或者左下角的元素a[row][col]与target进行比较，
-    //当target小于元素a[row][col]时，那么target必定在元素a所在列的左边,即col--；
-    //当target大于元素a[row][col]时，那么target必定在元素a所在行的下边,即row++
-    //O(n)
-    public static boolean Find(int target, int array[][]) {
-        int row = array.length;//行数
+    /**
+     * 矩阵是有序的：利用二维数组由上到下，由左到右递增的规律。
+     * 那么选取右上角或者左下角的元素a[row][col]与target进行比较，
+     * 当target小于元素a[row][col]时，那么target必定在元素a所在列的左边,即col--；
+     * 当target大于元素a[row][col]时，那么target必定在元素a所在行的下边,即row++
+     * O(n)
+     * @param target
+     * @param array
+     * @return
+     */
+    public static boolean find(int target, int[][] array) {
+        // 行数
+        int row = array.length;
         int col = array[0].length;
         if (col == 0 || target < array[0][0] || target > array[row - 1][col - 1]) {
             return false;
@@ -63,8 +67,8 @@ public class ArraySearch {
     public static void main(String[] args) {
 
         int[][] array = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
-        System.out.println(Find(7, array));
-        System.out.println(Find(1, array));
+        System.out.println(find(7, array));
+        System.out.println(find(1, array));
 
     }
 }

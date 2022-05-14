6b3c5969e76e779b1f1c345c0d8d4a4a0ea4c13e
dustdawn <dustdawn@163.com>
2022-05-14 14:29:42 +0800

    饿汉式单例模式

--- src/com/dustdawn/DesignPatterns/Creational/SingletonPattern/SingletonII.java
+++ src/com/dustdawn/DesignPatterns/Creational/SingletonPattern/SingletonII.java
@@ -1,14 +1,13 @@
 package com.dustdawn.DesignPatterns.Creational.SingletonPattern;
 
 /**
+ * 饿汉式单例模式
+ * 没有加锁，但浪费资源，执行效率高
+ *
  * @author dustdawn
  * @date 2019/11/5 14:48
  */
 
-/**
- * 饿汉式单例模式
- * 没有加锁，但浪费资源，执行效率高
- */
 public class SingletonII {
     private static SingletonII instance = new SingletonII();//静态私有成员变量
 
@@ -17,7 +16,7 @@ public class SingletonII {
     }
 
     //静态共有工厂方法，返回唯一实例
-    public static synchronized SingletonII getInstance() {
+    public synchronized static SingletonII getInstance() {
         return instance;
     }
 }

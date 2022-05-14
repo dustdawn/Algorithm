b6ca3b78ca4664af6582d5a557fb3c4c0468982f
dustdawn <dustdawn@163.com>
2022-05-14 14:26:58 +0800

    基本的二叉树节点

---
+++ src/com/dustdawn/leetcode/datastructure/TreeNode.java
@@ -0,0 +1,27 @@
+package com.dustdawn.leetcode.datastructure;
+
+/**
+ * 基本的二叉树节点
+ * @author dustdawn
+ * @date 2022/4/23 14:31
+ */
+public class TreeNode {
+    /**
+     * 节点存储的值
+     */
+    int val;
+    /**
+     * 指向左侧子节点的指针
+     */
+    TreeNode left;
+    /**
+     * 指向右侧子节点的指针
+     */
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}

2ba7b18d800774f8e7dfc22dbffa3ab809c89c22
dustdawn <dustdawn@163.com>
2022-05-15 15:13:49 +0800

    单链表节点类型

---
+++ src/com/dustdawn/leetcode/datastructure/ListNode.java
@@ -0,0 +1,22 @@
+package com.dustdawn.leetcode.datastructure;
+
+/**
+ * 单链表节点类型
+ * @author dustdawn
+ * @date 2022/5/15 15:12
+ */
+public class ListNode {
+    /**
+     * 节点存储的值
+     */
+    public int val;
+    /**
+     * 指向下一个节点的指针
+     */
+    ListNode next;
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}

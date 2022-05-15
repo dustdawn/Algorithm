7333697896aac3fa981f1ce123a757365fabf0d7
dustdawn <dustdawn@163.com>
2022-05-15 15:10:16 +0800

    111. 二叉树的最小深度（深度优先搜索）

--- src/com/dustdawn/leetcode/dfs/MinDepth.java
+++ src/com/dustdawn/leetcode/dfs/MinDepth.java
@@ -7,6 +7,9 @@ import com.dustdawn.leetcode.datastructure.TreeNode;
  * 给定一个二叉树，找出其最小深度。
  * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
  * 说明：叶子节点是指没有子节点的节点。
+ * 提示：
+ * 树中节点数的范围在 [0, 105] 内
+ * -1000 <= Node.val <= 1000
  *
  * @author dustdawn
  * @date 2022/5/11 20:23

1d37eed8af4fc5fc5bc9e49a5593d452ef89e2f6
dustdawn <dustdawn@163.com>
2022-05-15 11:07:36 +0800

    111. 二叉树的最小深度

---
+++ src/com/dustdawn/leetcode/datastructure/MinDepth.java
@@ -0,0 +1,73 @@
+package com.dustdawn.leetcode.datastructure;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 111. 二叉树的最小深度
+ * 给定一个二叉树，找出其最小深度。
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ * 说明：叶子节点是指没有子节点的节点。
+ *
+ * @author dustdawn
+ * @date 2022/5/11 20:23
+ */
+public class MinDepth {
+    public static int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        } else if (root.left == null || root.right == null) {
+            // 有一个子节点，说明不为叶子结点，继续往下递归
+            return Math.max(minDepth(root.left), minDepth(root.right)) + 1;
+        } else {
+            // 有两个子节点，判断左右子树的最小深度
+            return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+        }
+    }
+
+    /**
+     * 广度优先搜索
+     * 时间复杂度：O(N)O(N)，其中 NN 是树的节点数。对每个节点访问一次。
+     * 空间复杂度：O(N)O(N)，其中 NN 是树的节点数。空间复杂度主要取决于队列的开销，队列中的元素个数不会超过树的节点数。
+     *
+     * @param root
+     * @return
+     */
+    public static int minDepthByQueue(TreeNode root) {
+        Queue<QueueNode> queue = new LinkedList<>();
+        queue.offer(new QueueNode(root, 1));
+        while (!queue.isEmpty()) {
+            QueueNode poll = queue.poll();
+            TreeNode node = poll.node;
+            int depth = poll.depth;
+            // 遍历到叶子节点
+            if (node.left == null && node.right == null) {
+                return depth;
+            }
+            if (node.left != null) {
+                queue.offer(new QueueNode(node.left, depth + 1));
+            }
+            if (node.right != null){
+                queue.offer(new QueueNode(node.right, depth + 1));
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        /**
+         * 示例 1：
+         *
+         *
+         * 输入：root = [3,9,20,null,null,15,7]
+         * 输出：2
+         * 示例 2：
+         *
+         * 输入：root = [2,null,3,null,4,null,5,null,6]
+         * 输出：5
+         */
+    }
+}

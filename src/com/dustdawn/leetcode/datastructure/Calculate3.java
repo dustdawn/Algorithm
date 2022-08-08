9584e27e54b3533be9c9be604fcbd4b57566e9b0
dustdawn <dustdawn@163.com>
2022-08-08 16:26:11 +0800

    772. 基本计算器III

---
+++ src/com/dustdawn/leetcode/datastructure/Calculate3.java
@@ -0,0 +1,21 @@
+package com.dustdawn.leetcode.datastructure;
+
+/**
+ * 772. 基本计算器III（包含+-x/ 以及括号）
+ * 现基本计算器以计算简单表达式字符串。
+ * 表达式字符串可以包含左括号（和右括号）、加号+或减号、非负整数和空格。
+ * 表达式字符串只包含非负整数、+、-、*、/运算符、左括号和空格。整数除法应该截断为零。
+ * 您可以假定给定的表达式总是有效的。所有中间结果将在范围内[-2147483648，2147483647]
+ *
+ * @author dustdawn
+ * @date 2022/8/8 15:08
+ */
+public class Calculate3 {
+    public static int calculate(String s) {
+        return Calculate.calculate(s);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

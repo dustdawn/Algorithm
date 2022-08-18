09867b2e9ea11397e901fb24e687adcfd35cc14e
dustdawn <dustdawn@163.com>
2022-08-18 10:30:13 +0800

    简单选择排序

--- src/com/dustdawn/leetcode/sort/SelectSort.java
+++ src/com/dustdawn/leetcode/sort/SelectSort.java
@@ -2,7 +2,8 @@ package com.dustdawn.leetcode.sort;
 
 /**
  * 简单选择排序
- * 选择出最小的比较 O(n^2)
+ * 选择出最小的比较
+ * O(n^2) O(1) 不稳定
  *
  * @author dustdawn
  * @date 2022/4/3 15:56
@@ -13,7 +14,7 @@ public class SelectSort extends Sort {
             return nums;
         }
         // 每一轮都选择出最小的数，从前往后排
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length - 1; i++) {
             // 第i轮从未排好序的[i + 1, nums.length - 1]范围中选出最小数，与nums[i]进行交换
             int minIndex = i;
             for (int j = i + 1; j < nums.length; j++) {

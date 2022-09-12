773c2c3cbd3460f10ec0026da2d854856df6d0a1
dustdawn <dustdawn@163.com>
2022-09-12 14:14:23 +0800

    496. 下一个更大元素 I(简单)(单调栈+哈希表)

--- src/com/dustdawn/leetcode/datastructure/NextGreaterElement.java
+++ src/com/dustdawn/leetcode/datastructure/NextGreaterElement.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.Stack;
 
 /**
- * 496. 下一个更大元素 I
+ * 496. 下一个更大元素 I(简单)(单调栈+哈希表)
  * nums1 中数字 x 的 下一个更大元素 是指 x 在 nums2 中对应位置 右侧 的 第一个 比 x 大的元素。
  * 给你两个 没有重复元素 的数组 nums1 和 nums2 ，下标从 0 开始计数，其中nums1 是 nums2 的子集。
  * 对于每个 0 <= i < nums1.length ，找出满足 nums1[i] == nums2[j] 的下标 j ，并且在 nums2 确定 nums2[j] 的 下一个更大元素 。

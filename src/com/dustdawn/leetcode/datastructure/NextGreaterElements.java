75d34e852f010dcfdd15e3dec6c62ea4579e7609
dustdawn <dustdawn@163.com>
2022-09-12 14:20:13 +0800

    503. 下一个更大元素 II(中等)(单调栈+取模)

--- src/com/dustdawn/leetcode/datastructure/NextGreaterElements.java
+++ src/com/dustdawn/leetcode/datastructure/NextGreaterElements.java
@@ -3,7 +3,7 @@ package com.dustdawn.leetcode.datastructure;
 import java.util.Stack;
 
 /**
- * 503. 下一个更大元素 II
+ * 503. 下一个更大元素 II(中等)(单调栈+取模)
  * 给定一个循环数组 nums （ nums[nums.length - 1] 的下一个元素是 nums[0] ），返回 nums 中每个元素的 下一个更大元素 。
  * 数字 x 的 下一个更大的元素 是按数组遍历顺序，这个数字之后的第一个比它更大的数，这意味着你应该循环地搜索它的下一个更大的数。
  * 如果不存在，则输出 -1 。
@@ -15,17 +15,12 @@ import java.util.Stack;
  * @date 2022/6/6 20:23
  */
 public class NextGreaterElements {
-    /**
-     * 单调栈
-     *
-     * @param nums
-     * @return
-     */
     public static int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
         int[] res = new int[n];
         Stack<Integer> stack = new Stack<>();
         // 模拟数组长度翻倍
+        // 倒序，保证翻倍后的最后一个元素(2 * n - 1)默认为-1，后续在n - 1位置进行更新
         for (int i = 2 * n - 1; i >= 0; i--) {
             while (!stack.isEmpty() && stack.peek() <= nums[i % n]) {
                 stack.pop();

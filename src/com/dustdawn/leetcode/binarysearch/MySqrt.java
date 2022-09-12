a4421e93a35fefeeb274c4287571556d57732ecd
dustdawn <dustdawn@163.com>
2022-09-12 14:48:22 +0800

    69. x 的平方根(简单)(锁定右边界二分搜索)

--- src/com/dustdawn/leetcode/binarysearch/MySqrt.java
+++ src/com/dustdawn/leetcode/binarysearch/MySqrt.java
@@ -6,7 +6,7 @@ package com.dustdawn.leetcode.binarysearch;
  * 由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。
  * 注意：不允许使用任何内置指数函数和算符，例如 pow(x, 0.5) 或者 x ** 0.5 。
  * 提示：
- * 0 <= x <= 231 - 1
+ * 0 <= x <= 2^31 - 1
  *
  * @author dustdawn
  * @date 2022/9/12 14:22
